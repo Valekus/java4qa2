848192376ae2b9a50e1de48bb0c4856e8435818d
Valentin Usatiy <Valekus777@gmail.com>
2018-05-16 21:30:15 +0300

    Person edit

--- src/LR_5_hw/Person.java
+++ src/LR_5_hw/Person.java
@@ -5,8 +5,8 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class Person {
-    protected  String sName;
-    protected  Date birthdate;
+    protected String sName;
+    protected Date birthdate;
 
     public void setsName(String sName) {
         this.sName = sName;
@@ -16,42 +16,42 @@ public class Person {
         return sName;
     }
 
-    public int getAge(){
+    public int getAge() {
         Calendar today = Calendar.getInstance();
         Calendar birthday = new GregorianCalendar();
         birthday.setTime(birthdate);
 
         int yearDif = today.get(Calendar.YEAR) - birthday.get(Calendar.YEAR);
-        if (today.get(Calendar.MONTH)< birthday.get(Calendar.MONTH)){
+        if (today.get(Calendar.MONTH) < birthday.get(Calendar.MONTH)) {
             yearDif = yearDif - 1;
-        }
-        else if (today.get(Calendar.MONTH) == birthday.get(Calendar.MONTH)
-                && today.get(Calendar.DATE) < birthday.get(Calendar.DATE)){
+        } else if (today.get(Calendar.MONTH) == birthday.get(Calendar.MONTH)
+                && today.get(Calendar.DATE) < birthday.get(Calendar.DATE)) {
             yearDif = yearDif - 1;
         }
         return yearDif;
     }
 
-    public  void  setBirthDate(Date BirthDate){
+    public void setBirthDate(int iYear, int iMonth, int iDay) {
         Calendar cal = Calendar.getInstance();
-        cal.set(iYear,iMonth, iDay);
+        cal.set(iYear, iMonth, iDay);
         this.birthdate = cal.getTime();
     }
 
-    public Date getBirthDate(){
+    public Date getBirthDate() {
         return birthdate;
     }
 
-    public String toString(){
+    public String toString() {
         String s = "Name: " + getsName() + "\n";
-               s += " Age: " + getAge() + "\n";
+        s += " Age: " + getAge() + "\n";
         return s;
     }
+
     public static void main(String[] args) {
-            Person homer = new Person();
-            homer.setsName("Valentin Usatiy");
-            homer.setBirthDate(1986, Calendar.OCTOBER, 22);
-            System.out.println(homer);
-        }
+        Person homer = new Person();
+        homer.setsName("Valentin Usatiy");
+        homer.setBirthDate(1986, Calendar.OCTOBER, 22);
+        System.out.println(homer);
+    }
 
 }
